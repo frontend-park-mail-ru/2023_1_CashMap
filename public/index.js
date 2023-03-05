2071a6212791252bfd3f064a2aa862d4b393e8cf
WhoIsYgim <e.larkin3004@gmail.com>
2023-03-05 07:37:24 +0300

    Добавить иконки в боковое меню

--- public/index.js
+++ public/index.js
@@ -76,63 +76,63 @@ function renderSideBar(parent) {
 		pages: [
 				{
 					ref: '/profile',
-					iconPath: './static/img/nav_icons/profile.svg',
-					hoveredIconPath: '../html/img/nav-icons/profile_hover.svg',
+					iconPath: 'static/img/nav_icons/profile.svg',
+					hoveredIconPath: 'static/img/nav_icons/profile_hover.svg',
 					title: 'Моя страница',
 					notifies: 0
 				},
 
 				{
 					ref: '/feed',
-					iconPath: '../html/img/nav_icons/news.svg',
-					hoveredIconPath: '../html/img/nav-icons/news_hover.svg',
+					iconPath: 'static/img/nav_icons/news.svg',
+					hoveredIconPath: 'static/img/nav_icons/news_hover.svg',
 					title: 'Новости',
 					notifies: 0
 				},
 
 				{
 					ref: '/msg',
-					iconPath: '../html/img/nav_icons/messenger.svg',
-					hoveredIconPath: '../html/img/nav-icons/messenger_hover.svg',
+					iconPath: 'static/img/nav_icons/messenger.svg',
+					hoveredIconPath: 'static/img/nav_icons/messenger_hover.svg',
 					title: 'Мессенджер',
 					notifies: 0
 				},
 
 				{
 					ref: '/albums',
-					iconPath: '../html/img/nav_icons/photos.svg',
-					hoveredIconPath: '../html/img/nav-icons/photos_hover.svg',
+					iconPath: 'static/img/nav_icons/photos.svg',
+					hoveredIconPath: 'static/img/nav_icons/photos_hover.svg',
 					title: 'Фотографии',
 					notifies: 0
 				},
 
 				{
 					ref: '/friends',
-					iconPath: '../html/img/nav-icons/friends.svg',
-					hoveredIconPath: '../html/img/nav-icons/friends_hover.svg',
+					iconPath: 'static/img/nav_icons/friends.svg',
+					hoveredIconPath: 'static/img/nav_icons/friends_hover.svg',
 					title: 'Друзья',
 					notifies: 0
 				},
 
 				{
 					ref: '/groups',
-					iconPath: '../html/img/nav-icons/groups.svg',
-					hoveredIconPath: '../html/img/nav-icons/groups_hover.svg',
+					iconPath: 'static/img/nav_icons/groups.svg',
+					hoveredIconPath: 'static/img/nav_icons/groups_hover.svg',
 					title: 'Сообщества',
 					notifies: 0
 				},
 
 				{
 					ref: '/bookmarks',
-					iconPath: '../html/img/nav_icons/bookmarks',
-					hoveredIconPath: '../html/img/nav-icons/bookmarks_hover',
+					iconPath: 'static/img/nav_icons/bookmarks.svg',
+					hoveredIconPath: 'static/img/nav_icons/bookmarks_hover.svg',
 					title: 'Закладки',
 					notifies: 0
 				}
 				]
 			};
 
-	const sideBar = new SideBar(parent, 'Depeche', '../html/img/logo.svg', navItems.pages);
+	const sideBar = new SideBar(parent, 'Depeche', 'static/img/logo.svg', navItems.pages);
 
 	sideBar.render();
 } 
