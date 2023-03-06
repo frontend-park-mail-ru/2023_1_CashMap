ca8c3df6c6107a2301dc89ba1cb8df49c64c6f99
Banzay <bazaleevf@gmail.com>
2023-03-06 10:57:31 +0300

    Исправит валидацию почты

--- public/modules/validation.js
+++ public/modules/validation.js
@@ -111,8 +111,18 @@ function validateEmail(email) {
 	}
 
 	match = email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+	alert(match)
 
-	return {status: true} ? match != null : {status: false, error: 'invalid email'};
+	if (match == null) {
+		return {
+			status: false,
+			error: 'invalid email'
+		};
+	}
+
+	return {
+		status: true,
+	}
 }
 
 function validateName(name) {	
