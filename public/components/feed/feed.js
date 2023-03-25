7f92b34c98c8826015e9887701f6cc8ccd719fcf
Banzay <bazaleevf@gmail.com>
2023-03-26 00:04:26 +0300

    Изменить стили ленты

--- public/components/feed/feed.js
+++ public/components/feed/feed.js
@@ -7,13 +7,37 @@ export default class Feed {
 		this.#parent = parent;
 
 		this.#config = {
-			posts
+			sideBarData: {
+				logoImgPath: 'static/img/logo.svg',
+				logoText: 'Depeche',
+				buttons: [
+					{text: 'Моя страница', jsId: 'js-side-bar-my-page', iconPath: 'static/img/nav_icons/profile.svg'},
+					{text: 'Новости', jsId: 'js-side-bar-news', iconPath: 'static/img/nav_icons/news.svg'},
+					{text: 'Мессенджер', jsId: 'js-side-bar-msg', iconPath: 'static/img/nav_icons/messenger.svg'},
+					{text: 'Фотографии', jsId: 'js-side-bar-photo', iconPath: 'static/img/nav_icons/photos.svg'},
+					{text: 'Друзья', jsId: 'js-side-bar-friends', iconPath: 'static/img/nav_icons/friends.svg'},
+					{text: 'Сообщества', jsId: 'js-side-bar-groups', iconPath: 'static/img/nav_icons/groups.svg'},
+					{text: 'Закладки', jsId: 'js-side-bar-bookmarks', iconPath: 'static/img/nav_icons/bookmarks.svg'}]
+			},
+			headerData: {
+				profileUrl: '#',
+				avatar: 'static/img/post_icons/profile_image.svg',
+			},
+			postAreaData: {
+				post: posts,
+			},
 		};
 	}
 
 	render() {
+		Handlebars.registerPartial('inputField', Handlebars.templates.inputField)
+		Handlebars.registerPartial('button', Handlebars.templates.button)
+
+		Handlebars.registerPartial('sideBar', Handlebars.templates.sideBar)
+		Handlebars.registerPartial('header', Handlebars.templates.header)
+		Handlebars.registerPartial('postArea', Handlebars.templates.postArea)
+
 		const template = Handlebars.templates.feed;
-		
 		this.#parent.innerHTML = template(this.#config);
 	}
 
