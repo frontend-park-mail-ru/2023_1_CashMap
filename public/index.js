0cc59d6f55b6b518bee34e4c1d22fb7ce1466e1e
Banzay <bazaleevf@gmail.com>
2023-04-06 22:02:03 +0300

    Добавить запросы к серверу

--- public/index.js
+++ public/index.js
@@ -1,3 +1,22 @@
-import {initPage} from "./modules/goToPage.js";
+import Router from "./modules/router.js";
+import SignInView from "./views/signInView.js";
+import SignUpView from "./views/signUpView.js";
+import FeedView from "./views/feedView.js";
+import {actionUser} from "./actions/actionUser.js";
 
-initPage()
+
+const Views = {
+    FeedView: new FeedView(),
+    SignInView: new SignInView(),
+    SignUpView: new SignUpView(),
+};
+
+actionUser.checkAuth();
+
+Router.registerPage('/', Views.FeedView);
+Router.registerPage('/feed', Views.FeedView);
+Router.registerPage('/signIn', Views.SignInView);
+Router.registerPage('/signUp', Views.SignUpView);
+
+Router.init()
+
--- public/modules/ajax.js
+++ public/modules/ajax.js
@@ -1,7 +1,7 @@
 class Ajax {
     constructor() {
-        this._backendUrl = 'http://127.0.0.1';
-        //this._backendUrl = 'http://95.163.212.121';
+        //this._backendUrl = 'http://127.0.0.1';
+        this._backendUrl = 'http://95.163.212.121';
         this._backendPort = '8080';
 
         this._apiUrl = {
@@ -10,6 +10,8 @@ class Ajax {
             signOut: '/auth/logout',
             check: '/auth/check',
             feed: '/api/feed',
+            getUserInfo: '/api/user/profile',
+            friends: '',
         }
 
         this._requestType = {
@@ -20,7 +22,6 @@ class Ajax {
 
     _request(apiUrlType, requestType, body) {
         const requestUrl = this._backendUrl + ':' + this._backendPort + apiUrlType;
-
         console.log(requestUrl);
 
         return fetch(requestUrl, {
@@ -44,70 +45,29 @@ class Ajax {
         return this._request(this._apiUrl.signUp, this._requestType.POST, JSON.stringify({body}));
     }
 
-    async check() {
+    async signOut() {
+        return this._request(this._apiUrl.signOut, this._requestType.POST);
+    }
+
+    async getUserInfo(link) {
+        if (link === undefined) {
+            return this._request(this._apiUrl.getUserInfo, this._requestType.GET);
+        } else {
+            return this._request(this._apiUrl.getUserInfo + `?link=${link}`, this._requestType.GET);
+        }
+    }
+
+    async checkAuth() {
         return this._request(this._apiUrl.check, this._requestType.GET);
     }
 
-    async getPosts(count) {
+    async getPosts(count, offset) {
+        //return this._request(this._apiUrl.feed + `?batch_size=${count}&last_post_id=${offset}`, this._requestType.GET);
         return this._request(this._apiUrl.feed + `?batch_size=${count}`, this._requestType.GET);
     }
 
-
-
-
-    static async get(url) {
-        const response = await fetch(URL+url, {
-            method: AJAX_METHOD_TYPES.GET,
-            mode: "cors",
-            credentials: "include",
-            headers: {
-                Origin: this.FrontendHost,
-            },
-        });
-
-        const body = await response.json();
-
-        return {
-            status: response.status,
-            message: response.statusText,
-            body: body.body,
-        }
-    }
-
-    static async get_c(url) {
-        const response = await fetch(URL+url, {
-            method: AJAX_METHOD_TYPES.GET,
-            mode: "cors",
-            credentials: "include",
-            headers: {
-                Origin: this.FrontendHost,
-            },
-        });
-
-        //const body = await response.json();
-
-        return {
-            status: response.status,
-            message: response.statusText,
-            body: response,
-        }
-    }
-
-    static async post(url, body) {
-        const response = await fetch(URL+url, {
-            method: AJAX_METHOD_TYPES.POST,
-            mode: "cors",
-            credentials: "include",
-            headers: {
-                Origin: this.FrontendHost,
-            },
-            body: JSON.stringify({body} ),
-        });
-        return {
-            status: response.status,
-            message: response.statusText,
-            body: response.body,
-        }
+    async getFriends(count, offset) {
+        return this._request(this._apiUrl.friends + `?batch_size=${count}&last_post_id=${offset}`, this._requestType.GET);
     }
 }
 
