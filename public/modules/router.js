dd3764c7416978f06e629fc228e7bcf185ed844c
Banzay <bazaleevf@gmail.com>
2023-04-06 21:59:47 +0300

    Добавить роутинг

--- public/modules/router.js
+++ public/modules/router.js
@@ -1,10 +1,32 @@
 class Router {
     constructor() {
-
+        this.currentPage = null;
+        this._pages = {};
+        this.rootElement = document.getElementById('root');
     }
 
-    go() {
+    registerPage(url, view) {
+        this._pages[url] = view;
+    }
 
+    go(url) {
+        this.currentPage?.remove();
+
+        if (this._pages[url]) {
+            this.currentPage = this._pages[url];
+            this.currentPage.render();
+
+            if (window.location.pathname + window.location.search !== url) {
+                window.history.replaceState(null, null, url);
+            }
+
+        } else {
+            alert('page not found');
+        }
+    }
+
+    init() {
+        this.go(window.location.pathname + window.location.search);
     }
 }
 
