2044dcfe160f0d5adb3189f148f2c870a66b02b2
ArKarina <anohina.karina@bk.ru>
2023-04-07 19:07:55 +0300

    Добавить страницы

--- public/views/profileView.js
+++ public/views/profileView.js
@@ -2,17 +2,17 @@ import userStore from "../stores/userStore.js";
 import Router from "../modules/router.js";
 import {sideBarConst, headerConst} from "../static/htmlConst.js";
 import {actionUser} from "../actions/actionUser.js";
-import {actionFriends} from "../actions/actionFriends.js";
-import friendsStore from "../stores/friendsStore.js";
+import {actionPost} from "../actions/actionPost.js";
+import postsStore from "../stores/postsStore.js";
 
-export default class FriendsView {
+export default class ProfileView {
 	constructor() {
 		this._addHandlebarsPartial();
 
-		this._jsId = 'friends';
+		this._jsId = 'profile';
 		this.curPage = false;
 
-		friendsStore.registerCallback(this.updatePage.bind(this));
+		postsStore.registerCallback(this.updatePage.bind(this));
 		userStore.registerCallback(this.updatePage.bind(this));
 	}
 
@@ -22,8 +22,12 @@ export default class FriendsView {
 		Handlebars.registerPartial('sideBar', Handlebars.templates.sideBar)
 		Handlebars.registerPartial('header', Handlebars.templates.header);
 		Handlebars.registerPartial('menuItem', Handlebars.templates.menuItem)
-		Handlebars.registerPartial('search', Handlebars.templates.search)
-		Handlebars.registerPartial('friend', Handlebars.templates.friend)
+		Handlebars.registerPartial('profileCard', Handlebars.templates.profileCard)
+		Handlebars.registerPartial('postArea', Handlebars.templates.postArea)
+		Handlebars.registerPartial('post', Handlebars.templates.post)
+		Handlebars.registerPartial('createPost', Handlebars.templates.createPost)
+		Handlebars.registerPartial('commentArea', Handlebars.templates.commentArea)
+		Handlebars.registerPartial('comment', Handlebars.templates.comment)
 	}
 
 	_addPagesElements() {
@@ -43,25 +47,25 @@ export default class FriendsView {
 
 	updatePage() {
 		if (this.curPage) {
-			//alert('friends');
+			//alert('profile');
 			if (!userStore.user.isAuth) {
 				Router.go('/signIn');
 			} else {
-				console.log(22222)
+				console.log(4444444)
 				this._render();
 			}
 		}
 	}
 
 	showPage() {
-		//alert('show friends')
+		//alert('show profile')
 		if (userStore.user.isAuth === false) {
 			console.log(userStore.user.isAuth);
 			Router.go('/signIn');
 		} else {
 			actionUser.getUserInfo();
-			actionFriends.getFriends(15, 0);
-			console.log(11111)
+			actionPost.getPostsByUser('id1', 10);
+			console.log(11133311)
 			this._render();
 		}
 	}
@@ -70,11 +74,31 @@ export default class FriendsView {
 		let header = headerConst;
 		header['avatar'] = userStore.user.avatar;
 
-		const template = Handlebars.templates.friends;
+		const template = Handlebars.templates.profile;
 		Router.rootElement.innerHTML = template({
 			sideBarData: sideBarConst,
 			headerData: header,
-			friendsData: friendsStore.friends,
+			//profileData: userStore.user,
+			profileData: {
+                id: 1,
+				avatar: 'static/img/post_icons/profile_image.svg',
+				firstName: 'Карина',
+				lastName: 'Анохина',
+				status: 'Это мой статус)))',
+				birthday: '01.01.2000'
+            },
+			// this.user = {
+			// 	isAuth: false,
+			// 	errorAuth: '',
+			// 	errorReg: '',
+	
+			// 	link: null,
+			// 	firstName: null,
+			// 	lastName: null,
+			// 	email: null,
+			// 	avatar: null,
+			// };
+			postAreaData: {createPostData: {avatar: userStore.user.avatar}, postList: postsStore.posts},
 		});
 
 		this._addPagesElements();
