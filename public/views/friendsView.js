5e644214b3b91850388141b968d5236bf474b2c7
Анохина Карина <102531036+ArKarina@users.noreply.github.com>
2023-04-25 20:54:22 +0300

    Cm 3.5 починить добавления в друзья (#49)
    * Изменить компоненты друзей
    
    * Изменить компоненты настроек
    
    * Изменить страницу друзей
    
    * Исправить замечания

--- public/views/friendsView.js
+++ public/views/friendsView.js
@@ -32,10 +32,26 @@ export default class FriendsView extends BaseView {
 	_addPagesElements() {
 		this._exitBtn = document.getElementById('js-exit-btn');
 		this._settingsBtn = document.getElementById('js-settings-btn');
+		this._feedBtn = document.getElementById('js-logo-go-feed');
 		this._friendsBtn = document.getElementById('js-menu-friends');
 		this._subscribersBtn = document.getElementById('js-menu-subscribers');
 		this._subscriptionsBtn = document.getElementById('js-menu-subscriptions');
 		this._findFriendsBtn = document.getElementById('js-menu-find-friends');
+
+		switch (window.location.pathname) {
+			case '/friends':
+				this._friendsBtn.style.color = activeColor;
+				break;
+			case '/subscribers':
+				this._subscribersBtn.style.color = activeColor;
+				break;
+			case '/subscriptions':
+				this._subscriptionsBtn.style.color = activeColor;
+				break;
+			case '/findFriends':
+				this._findFriendsBtn.style.color = activeColor;
+				break;
+		}
 
 		switch (window.location.pathname) {
 			case '/friends':
@@ -113,6 +129,24 @@ export default class FriendsView extends BaseView {
 		this._feedBtn.addEventListener('click', () => {
             Router.go('/feed', false);
         });
+		this._friendsBtn.addEventListener('click', () => {
+			Router.go('/friends', false);
+		});
+
+		this._subscribersBtn.addEventListener('click', () => {
+			this._subscribersBtn.style.color = activeColor;
+			Router.go('/subscribers', false);
+		});
+
+		this._subscriptionsBtn.addEventListener('click', () => {
+			this._subscriptionsBtn.style.color = activeColor;
+			Router.go('/subscriptions', false);
+		});
+
+		this._findFriendsBtn.addEventListener('click', () => {
+			this._findFriendsBtn.style.color = activeColor;
+			Router.go('/findFriends', false);
+		});
 
 		for (let i = 0; i < this._addUser.length; i++) {
 			this._addUser[i].addEventListener('click', () => {
