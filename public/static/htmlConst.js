41cc3b4797dbbefcdff9ca683e61622ac15bfecc
Banzay <bazaleevf@gmail.com>
2023-04-27 01:12:35 +0300

    Поправить статику

--- public/static/htmlConst.js
+++ public/static/htmlConst.js
@@ -23,13 +23,13 @@ export const logoDataSignIn = {
     logoImgPath: 'static/img/logo.svg',
     backgroundImgPath: 'static/img/background_right.svg',
     logoText: 'Depeche',
-    logoTagline: 'Твоя социальная сеть 💩',
+    logoTagline: 'Твоя социальная сеть',
 };
 export const logoDataSignUp = {
     logoImgPath: 'static/img/logo.svg',
     backgroundImgPath: 'static/img/background_left.svg',
     logoText: 'Depeche',
-    logoTagline: 'Твоя социальная сеть 💩',
+    logoTagline: 'Твоя социальная сеть',
 };
 
 export const signInData = {
