bfbd0ca64013c1bd3d743d6415bebdbb2a202ec3
BazaleevFedor <BazaleevFedor>
2023-05-16 11:27:03 +0300

    static

--- public/stores/imgStore.js
+++ public/stores/imgStore.js
@@ -35,7 +35,7 @@ class imgStore {
         const response = await request.json();
 
         if (request.status === 200) {
-            const newUrl = `static/download?name=${ response.body.form[0].name }&type=${ response.body.form[0].type }`;
+            const newUrl = `static-service/download?name=${ response.body.form[0].name }&type=${ response.body.form[0].type }`;
             callback(newUrl);
         } else {
             alert('uploadImg error');
