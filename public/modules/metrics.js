f7d8f13a404998117399c8d58743de3aeb9c5e58
BazaleevFedor <bazaleevf@gmail.com>
2023-06-07 11:55:21 +0300

    add metric

--- public/modules/metrics.js
+++ public/modules/metrics.js
@@ -9,11 +9,17 @@ class Metrics {
     addMetric(url) {
         const perfNavigation = window.performance.getEntriesByType("navigation")[0];
         const resourceEntries = window.performance.getEntriesByType("resource");
+        const fetchEntries = window.performance.getEntriesByType("fetch");
         const memoryInfo = window.performance.memory;
 
         let totalLoadTime = 0;
         resourceEntries.forEach((entry) => {
             totalLoadTime += entry.duration;
+        });
+
+        let totalLoadTimeFetch = 0;
+        fetchEntries.forEach((entry) => {
+            totalLoadTimeFetch += entry.duration;
         });
 
         const metricsList = [
@@ -22,7 +28,8 @@ class Metrics {
             {name: 'Время установки соединения', metricValue: perfNavigation.connectEnd - perfNavigation.connectStart  + " мс"},
             {name: 'Время отправки запроса', metricValue: perfNavigation.responseStart - perfNavigation.requestStart  + " мс"},
             {name: 'Время получения ответа', metricValue: perfNavigation.responseEnd - perfNavigation.responseStart  + " мс"},*/
-            {name: 'Новых запросов к api', metricValue: Number(localStorage.getItem('reqCount'))},
+            {name: 'Новых запросов к api', metricValue: fetchEntries.length, metricValue2: fetchEntries},
+            {name: 'Время ожидания api запросов', metricValue: totalLoadTimeFetch + 'мс'},
             {name: 'Новых запросов ресурсов', metricValue: resourceEntries.length, metricValue2: resourceEntries },
             {name: 'Время подгрузки ресурсов страницы', metricValue: totalLoadTime  + " мс"},
             {name: 'Выделенная память', metricValue: (memoryInfo.usedJSHeapSize / (1024 * 1024)).toFixed(2) + 'МБ'},
