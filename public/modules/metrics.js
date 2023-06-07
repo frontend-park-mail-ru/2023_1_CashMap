e0a510ac8a1590b51d4562a3bddde311a2a3c674
BazaleevFedor <bazaleevf@gmail.com>
2023-06-07 12:22:26 +0300

    add metric

--- public/modules/metrics.js
+++ public/modules/metrics.js
@@ -8,9 +8,19 @@ class Metrics {
 
     addMetric(url) {
         const perfNavigation = window.performance.getEntriesByType("navigation")[0];
-        const fetchEntries = window.performance.getEntriesByType("fetch");
-        const resourceEntries = window.performance.getEntriesByType("resource");
+        const entries = window.performance.getEntriesByType("resource");
         const memoryInfo = window.performance.memory;
+
+        let fetchEntries = [];
+        let resourceEntries = [];
+
+        entries.forEach((request) => {
+            if (request.initiatorType === 'fetch') {
+                fetchEntries.push(request);
+            } else {
+                resourceEntries.push(request);
+            }
+        });
 
         let totalLoadTime = 0;
         resourceEntries.forEach((entry) => {
@@ -28,6 +38,8 @@ class Metrics {
             {name: 'Время ожидания api запросов', metricValue: totalLoadTimeFetch + 'мс'},
             {name: 'Новых запросов ресурсов', metricValue: resourceEntries.length, metricValue2: resourceEntries },
             {name: 'Время подгрузки ресурсов страницы', metricValue: totalLoadTime  + " мс"},
+            {name: 'Всего запросов для данной страницы', metricValue: entries.length },
+            {name: 'Общее время ожидание ресурсов', metricValue: totalLoadTimeFetch + totalLoadTime  + " мс"},
             {name: 'Выделенная память', metricValue: (memoryInfo.usedJSHeapSize / (1024 * 1024)).toFixed(2) + 'МБ'},
         ];
 
