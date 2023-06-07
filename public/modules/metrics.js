264b6fb33a5626ba40f481cd66ef18e36982ebcc
BazaleevFedor <bazaleevf@gmail.com>
2023-06-07 11:46:04 +0300

    add metric

--- public/modules/metrics.js
+++ public/modules/metrics.js
@@ -11,20 +11,24 @@ class Metrics {
         const resourceEntries = window.performance.getEntriesByType("resource");
         const memoryInfo = window.performance.memory;
 
+        let totalLoadTime = 0;
+        resourceEntries.forEach((entry) => {
+            totalLoadTime += entry.duration;
+        });
+
         const metricsList = [
             {name: 'Загружаемая страница', metricValue: url, metricValue2: Router.currentPage},
-            /*{name: '', metricValue: ''},
-            {name: 'Время загрузки страницы', metricValue: perfNavigation.loadEventEnd - perfNavigation.navigationStart  + " мс"},
+            /*{name: 'Время загрузки страницы', metricValue: perfNavigation.loadEventEnd - perfNavigation.navigationStart  + " мс"},
             {name: 'Время установки соединения', metricValue: perfNavigation.connectEnd - perfNavigation.connectStart  + " мс"},
             {name: 'Время отправки запроса', metricValue: perfNavigation.responseStart - perfNavigation.requestStart  + " мс"},
             {name: 'Время получения ответа', metricValue: perfNavigation.responseEnd - perfNavigation.responseStart  + " мс"},*/
-            {name: '', metricValue: ''},
-            {name: 'Всего запросов', metricValue: localStorage.getItem('reqCount')},
-            {name: 'Всего запросов', metricValue: resourceEntries.length},
-            {name: '', metricValue: ''},
+            {name: 'Всего запросов к api', metricValue: localStorage.getItem('reqCount')},
+            {name: 'Время подгрузки ресурсов страницы', metricValue: totalLoadTime  + " мс"},
+            {name: 'Новых запросов', metricValue: resourceEntries.length},
             {name: 'Выделенная память', metricValue: (memoryInfo.usedJSHeapSize / (1024 * 1024)).toFixed(2) + 'МБ'},
-            {name: '', metricValue: ''},
         ];
+
+        window.performance.clearResourceTimings();
 
         console.log(metricsList);
         this.metrics.push(metricsList);
